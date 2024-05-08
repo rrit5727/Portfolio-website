eb0d6a62b6aa22bf7fdddadb195bea1a48f4f6a4
rrit5727 <rrit5727@gmail.com>
2024-05-08 23:05:40 +1000

    style about section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,11 @@ import {BsFillMoonStarsFill} from 'react-icons/bs';
 import {AiFillLinkedin} from "react-icons/ai";
 import headshot from '../../public/rr-head-shot.jpg';
 import deved from '../../public/dev-ed-wave.png';
+import excel from '../../public/excel.png';
+import excel2 from '../../public/excel2.png';
+import excel3 from '../../public/excel3.jpeg';
+import excel5 from '../../public/excel5.png';
+import coding from '../../public/coding.png';
 import design from '../../public/design.png';
 import code from '../../public/code.png';
 import consulting from '../../public/consulting.png';
@@ -43,7 +48,19 @@ export default function Home() {
                 <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl"/>
               </li>
               <li>
-                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a>
+                <a className=" text-black  px-4 py-2 rounded-md ml-8" href="/#intro">Intro</a>
+              </li>
+              <li>
+                <a className=" text-black  px-4 py-2 rounded-md ml-8" href="/#about">About</a>
+              </li>
+              <li>
+                <a className=" text-black  px-4 py-2 rounded-md ml-8" href="/#projects">Projects</a>
+              </li>
+              <li>
+                <a className=" text-black  px-4 py-2 rounded-md ml-8" href="/#skills">Skills</a>
+              </li>
+              <li>
+                <a className=" text-black  px-4 py-2 rounded-md ml-8" href="#">Resume</a>
               </li>
             </ul>
           </nav>
@@ -56,9 +73,9 @@ export default function Home() {
               <h3 className="text-2xl py-2 md:text-3xl dark:text-white">Junior Software Developer</h3>
               <h4 className="text-1xl py-2 md:text-2xl dark:text-white">In Sydney</h4>
             </div>
-            <div className="w-1">              
+            <div className="w-20">              
             </div>
-            <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 overflow-hidden p-10" >
+            <div className="relative  bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 overflow-hidden p-10" >
               <div className="rounded-full overflow-hidden border-8 border-black border-solid">
                 <Image src={headshot} layout="fill" objectFit="cover"/>
               </div>
@@ -71,71 +88,20 @@ export default function Home() {
 
         {/* 2nd page here */}
 
-        <section id="about" className=" h-screen snap-start pt-20">
+        <section id="About" className=" h-screen snap-start pt-20">
           <div>
-            <h3 className="text-3xl py-2 dark:text-white" >Services offered:</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a developer, I have done remote work for 
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500"> startups </span>
-              and collaborated with talented people to create digital products 
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-80 dark:text-gray-200">
-              I offer a wide range of services, including brand-design, programming and teaching.
-            </p>
+            <h3 className="text-3xl py-1 dark:text-white">About Me</h3>            
           </div>
-          <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <div className="flex justify-center">
-                <Image src={design} width={100} height={100} />
-              </div>
-              <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
-              <p className="py-2">
-                Creating elegant designs suited for your needs following core 
-                desing theory
-              </p>
-              <h4 py-4 className="text-teal-600">Design tools I use:</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-            </div>
-
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <div className="flex justify-center">
-                <Image src={code} width={100} height={100} />
-              </div>
-              <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
-              <p className="py-2">
-                Creating elegant designs suited for your needs following core 
-                desing theory
-              </p>
-              <h4 py-4 className="text-teal-600">Design tools I use:</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-            </div>
-            
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <div className="flex justify-center">
-                <Image src={consulting} width={100} height={100} />
-              </div>
-              <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
-              <p className="py-2">
-                Creating elegant designs suited for your needs following core 
-                desing theory
-              </p>
-              <h4 py-4 className="text-teal-600">Design tools I use:</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-            </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap h-4/5 overflow-hidden">
+            <div className="basis-1/3 flex-1">
+              <Image src={excel5} className="rounded-lg object-cover " ></Image>
+            </div>            
           </div>
         </section>
 
-        <section id="portfolio" className=" h-screen snap-start pt-20">
+        <section id="projects" className=" h-screen snap-start pt-20">
           <div>
-            <h3 className="text-3xl py-1 dark:text-white">Portfolio</h3>
+            <h3 className="text-3xl py-1 dark:text-white">Projects</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Since the beginning of my journey as a developer, I have done remote work for 
               <span className="text-teal-500"> agencies </span>
@@ -153,19 +119,13 @@ export default function Home() {
             </div>                    
             <div className="basis-1/3 flex-1">
               <Image src={web2} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"></Image>
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image src={web3} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"></Image>
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image src={web4} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"></Image>
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image src={web5} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"></Image>
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image src={web6} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"></Image>
-            </div>                            
+            </div>            
+          </div>
+        </section>
+        
+        <section id="skills" className=" h-screen snap-start pt-20">
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white">Skills</h3>                                        
           </div>
         </section>
         </div>
