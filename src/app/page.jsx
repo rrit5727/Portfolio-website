8f2aef91959228efca932a16c947ff5fc97c6a48
rrit5727 <rrit5727@gmail.com>
2024-05-09 22:08:35 +1000

    add Projects section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,13 +14,46 @@ import web5 from '../../public/web5.png';
 import web6 from '../../public/web6.png';
 import { useClient } from 'next/client';
 import { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import {RxDotFilled} from 'react-icons/rx'
 
 
 
 export default function Home() {
   
-  
+  const slides = [
+    {
+      url: 'https://raw.githubusercontent.com/tdha/project-three/main/Readme_imgs/Results.png', 
+    },
+    {
+      url: 'https://raw.githubusercontent.com/rrit5727/project4/main/Readme_imgs/Home_page.png',
+    },
+    {
+      url: 'https://raw.githubusercontent.com/rrit5727/project4/main/Readme_imgs/Results.png',
+    },           
+    {
+      url: 'https://raw.githubusercontent.com/rrit5727/Project2/main/Readme_images/Item-list.png',
+    }           
+  ]
+  const [currentIndex, setCurrentIndex] = useState(1)
   const [darkMode, setDarkMode] = useState(false);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex -1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length -1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex)
+  }
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  }
+
   return (
     <div className={darkMode ? "dark" : ""}>
       
@@ -34,33 +67,36 @@ export default function Home() {
       <main className="bg-white  dark:bg-gray-900 relative">
         <div className="h-screen  snap-y snap-mandatory overflow-scroll px-10 md:px-20 lg:-px-40" >
         
-          <nav className="py-5 mb-12 flex justify-between dark:text-white sticky top-0 z-50 bg-white">
+          <nav className="py-5 mb-12 flex justify-between dark:text-white sticky top-0 z-50 bg-white dark:bg-gray-900">
             <h1 className="text-xl font-burtons">R/R</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl"/>
               </li>
               <li>
-                <a className=" text-black  px-4 py-2 rounded-md ml-8" href="/#intro">Intro</a>
+                <a className=" text-black dark:text-white  px-4 py-2 rounded-md ml-8" href="/#intro">Intro</a>
               </li>
               <li>
-                <a className=" text-black  px-4 py-2 rounded-md ml-8" href="/#about">About</a>
+                <a className=" text-black dark:text-white  px-4 py-2 rounded-md ml-8" href="/#about">About</a>
               </li>
               <li>
-                <a className=" text-black  px-4 py-2 rounded-md ml-8" href="/#projects">Projects</a>
+                <a className=" text-black  dark:text-white px-4 py-2 rounded-md ml-8" href="/#projects">Projects</a>
               </li>
               <li>
-                <a className=" text-black  px-4 py-2 rounded-md ml-8" href="/#skills">Skills</a>
+                <a className=" text-black dark:text-white  px-4 py-2 rounded-md ml-8" href="/#skills">Skills</a>
               </li>
               <li>
-                <a className=" text-black  px-4 py-2 rounded-md ml-8" href="#">Resume</a>
+                <a className=" text-black dark:text-white  px-4 py-2 rounded-md ml-8" href="#">Resume</a>
               </li>
             </ul>
           </nav>
-          
+
+
+
+
         
-        <section id="intro" className=" h-screen snap-start pt-40 py-20">
-          <div className="relative flex flex-col md:flex-row space-y-5 items-center justify-center text-center md:text-left md:max-h-fit">
+        <section id="intro" className=" snap-start pt-40 py-20 flex-row justify-center h-4/5 mb-40">
+          <div className="pt-20 relative flex flex-col md:flex-row space-y-5 items-center justify-center text-center md:text-left md:max-h-fit">
             <div className="text-left p-10">
               <h4 className="text-2xl py-2 md:text-3xl dark:text-white">Hello, my name is </h4>
               <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl dark:text-teal-400">Ross Ritchie</h2>
@@ -89,33 +125,33 @@ export default function Home() {
           </div>                    
         </section>
 
-        {/* 2nd page here */}
+        <section id="projects" className="h-screen snap-start pt-40 py-20">
+          <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group" >
+            <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className="w-full h-full rounded-2xl bg-center bg-cover duration-500">
+            {/* Left Arrow */}
+          <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2-xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+            <BsChevronCompactLeft onClick={prevSlide} size={30}/>
+          </div>
+            {/* Right Arrow */}
+          <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2-xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+            <BsChevronCompactRight onClick={nextSlide} size={30}/>
+          </div>
+            </div>
+            <div className="flex top-4 justify-center py-2">
+              {slides.map((slide, slideIndex) => (
+                <div 
+                  key={slideIndex} 
+                  onClick={() => goToSlide(slideIndex)} 
+                  className="text-2xl cursor-pointer">
+                  <RxDotFilled />
+                </div>
+              ))}
+            </div>
+          </div>  
+        </section>   
 
 
-        <section id="projects" className=" h-screen snap-start pt-20">
-          <div>
-            <h3 className="text-3xl py-1 dark:text-white">Projects</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a developer, I have done remote work for 
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500"> startups </span>
-              and collaborated with talented people to create digital products 
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer a wide range of services, including brand-design, programming and teaching.
-            </p>
-          </div>
-          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1">
-              <Image src={web1} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"></Image>
-            </div>                    
-            <div className="basis-1/3 flex-1">
-              <Image src={web2} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"></Image>
-            </div>            
-          </div>
-        </section>
-        
+                
         <section id="skills" className=" h-screen snap-start pt-20">
           <div>
             <h3 className="text-3xl py-1 dark:text-white">Skills</h3>                                        
